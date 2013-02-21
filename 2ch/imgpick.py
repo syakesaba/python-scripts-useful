620d6ae76fb4f29f51e800e1f176103e645d30ba
syakesaba <syakesaba@gmail.com>
2013-02-22 00:05:01 +0900

    手直し

--- 2ch/imgpick.py
+++ 2ch/imgpick.py
@@ -1,3 +1,6 @@
+#/usr/bin/env python3
+# encoding: utf-8
+
 import re,httplib2,bs4,os
 URI=input("URI:")
 data=httplib2.Http().request(URI)[1].decode("cp932")
@@ -6,19 +9,19 @@ tree=bs4.BeautifulSoup(data)
 atags=tree.findAll("a")
 uris=[]
 for atag in atags:
-	if atag["href"]:
-		if re.search("\.(jpe?g|gif|png)",atag["href"]):
-			uris.append(re.sub("/ime\.nu","",atag["href"]))
+    if atag["href"]:
+        if re.search("\.(jpe?g|gif|png)",atag["href"]):
+            uris.append(re.sub("/ime\.nu","",atag["href"]))
 print(uris)
 if uris:
-	dirname="X" + str(hash(URI))
-	os.mkdir(dirname)
+    dirname="X" + str(hash(URI))
+    os.mkdir(dirname)
 for uri in uris:
-	head,data=httplib2.Http().request(uri)
-	if head["status"] != "200":
-		print("[*]Passed %s" % uri)
-		continue
-	f=open("%s/X%s.jpg" % (dirname,str(hash(uri))),"wb")
-	f.write(data)
-	f.close()
-	print("%s has been downloaded" % uri)
+    head,data=httplib2.Http().request(uri)
+    if head["status"] != "200":
+        print("[*]Passed %s" % uri)
+        continue
+    f=open("%s/X%s.jpg" % (dirname,str(hash(uri))),"wb")
+    f.write(data)
+    f.close()
+    print("%s has been downloaded" % uri)
